cf016503890dc191a0843c5bccd1eecdea5e5c6e
madelinben <ben.madelin@ntlworld.com>
2023-08-22 17:04:22 +0100

    Configured repo with laravel and vue.

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+
+import Dashboard from './components/dashboard.vue';
+
+const app = createApp({
+    components: {
+        Dashboard, 
+    }
+});
+
+app.mount('#app');
---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="routing-header">
+        <h1>Welcome to the Dashboard!</h1>
+    </div>
+</template>
